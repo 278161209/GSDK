6f8fa1c4fc2916394bdc4c6cac9ff88a4b0d482f
gaochao_xiaoyun <gaochao521>
2020-09-28 09:34:23 +0800

    测试

---
+++ testlibrary/src/main/java/com/smxxy/testlibrary/TestUtil.java
@@ -0,0 +1,16 @@
+package com.smxxy.testlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * author : chao
+ * date   : 2020/9/27 11:30
+ * desc   :
+ */
+public class TestUtil {
+
+    public static void showMsg(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
